ce92ce669b88ad8752a4555c8bdedf4c04465d4b
Ba...na...na <mynguyen1594@gmail.com>
2015-01-28 17:25:36 -0800

    Remove and Rewrite Some Functions

--- ParkingTicket.java
+++ ParkingTicket.java
@@ -1,5 +1,65 @@
 package parkingTicketSimulator;
 
-public class ParkingTicket {
+//import parkingTicketSimulator.ParkingTicketSimulator.ParkedCar;
+//import project_2.Project_2.ParkingMeter;
 
+public class ParkingTicket {
+	ParkedCar parkedCar;
+	ParkingMeter parkingMeter;
+	PoliceOfficer policeOfficer;
+	
+	// Constructor
+	public ParkingTicket (ParkedCar ParkedCar, ParkingMeter ParkingMeter, PoliceOfficer PoliceOfficer) {
+		this.parkedCar = ParkedCar;
+		this.parkingMeter = ParkingMeter;
+		this.policeOfficer = PoliceOfficer;
+	}
+	
+	public String reportMaker() {
+		return this.parkedCar.getMaker();
+	}
+	
+	public String reportModel() {
+		return this.parkedCar.getModel();
+	}
+	
+	public String reportColor() {
+		return this.parkedCar.getColor();
+	}
+	
+	public String reportLicenseNumber() {
+		return this.parkedCar.getLisenceNumber();
+	}
+	
+	public double reportFine () {
+		double parkedFine = 0;
+		
+		// The violated time is the difference between the parked time and the time purchased.
+		double violatedTime = parkedCar.getParkedTime() - parkingMeter.getPurchasedTime();
+		double hrs = violatedTime/60;	// Convert to hours
+		double min = violatedTime % 60;	// Convert to minutes
+		
+		if (hrs <= 1) {	// For parking inappropriately equal or under 1 hour 
+			parkedFine = 25;
+		}
+		// Otherwise, the fine equals to $25 for the 1st hour plus $10 for each additional hours
+		else {	
+				int roundDwnHrs = (int)hrs;	// Round down the hours
+				
+				if (min == 0) {
+					roundDwnHrs -= 1;	// minus 1 if there is no minute (whole hours)
+				}
+				parkedFine = 25 + roundDwnHrs*10;
+		}
+		
+		return parkedFine;
+	}
+	
+	public String reportOfficerName() {
+		return this.policeOfficer.getName();
+	}
+	
+	public int reportOfficerBadgeNum () {
+		return this.policeOfficer.getBadgeNumber();
+	}
 }
--- PoliceOfficer.java
+++ PoliceOfficer.java
@@ -1,12 +1,12 @@
 package parkingTicketSimulator;
 
 public class PoliceOfficer {
-		String name;
-		int badgeNumber;
+		private String name;
+		private int badgeNumber;
 
 		PoliceOfficer() {}
 
-		PoliceOfficer(String Name, int BadgeNum) {
+		public PoliceOfficer(String Name, int BadgeNum) {
 			name = Name;
 			badgeNumber = BadgeNum;
 		}
@@ -27,4 +27,35 @@ public class PoliceOfficer {
 			this.badgeNumber = badgeNumber;
 		}
 		
+		public boolean examineParkedCar(ParkedCar parkedCar, ParkingMeter parkingMeter) {
+			boolean issueTicket = false;
+			
+			if (parkedCar.getParkedTime() > parkingMeter.getPurchasedTime()) {
+				issueTicket = true;
+			}
+			else {
+				issueTicket = false;
+			}
+			return issueTicket;
+		}
+		
+		public void examineAndIssueTicket(ParkedCar parkedCar, ParkingMeter parkingMeter) {
+			ParkingTicket parkingTicket = new ParkingTicket (parkedCar, parkingMeter, this);
+			
+			if (parkedCar.getParkedTime() > parkingMeter.getPurchasedTime()) {
+				System.out.printf("\t\t PARKING TICKET \n");
+				System.out.printf("**************************************************\n");
+				System.out.printf("License Number: " + parkingTicket.reportLicenseNumber()
+								+ "\t Color: " + parkingTicket.reportColor() + "\n");
+				System.out.printf("Maker: " + parkingTicket.reportMaker()
+								+ "\t Model: " + parkingTicket.reportModel() + "\n");
+				System.out.printf("Fine Amount: $%1.2f \n", parkingTicket.reportFine());
+				System.out.printf("Police Officer: " + parkingTicket.reportOfficerName()
+								+ "\nBadge Number: " + parkingTicket.reportOfficerBadgeNum());
+				
+			}
+			else {
+				System.out.printf("*** Ticket is not issued *** \n");
+			}
+		}
 }
